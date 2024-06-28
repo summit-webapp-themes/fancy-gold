dc3bc92d5fe1986f9343ae622b898379b7f2c554
karan <karan@8848digital.com>
2024-06-28 11:51:17 +0530

    fix(app.tsx): import string changes in _app.tsx file

--- _app.tsx
+++ _app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from '../store/store';
-import Layout from './components/Layout';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
