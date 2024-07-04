0cf1d85efe976e8a5028b0e332cee33ed647556f
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-07-04 18:31:04 +0530

    fix:pop over UI

--- components/Navbar/HeaderCategories.tsx
+++ components/Navbar/HeaderCategories.tsx
@@ -6,10 +6,7 @@ const HeaderCategories = ({ navbarData }: any) => {
   const [target, setTarget] = useState<HTMLElement | null>(null);
   const ref = useRef<HTMLDivElement | null>(null);
 
-  const handleMouseEnter = (
-    e: React.MouseEvent<HTMLElement>,
-    index: number
-  ) => {
+  const handleMouseEnter = (e: React.MouseEvent<HTMLElement>, index: number) => {
     setTarget(e.currentTarget);
     setShowPopoverIndex(index);
   };
@@ -18,44 +15,51 @@ const HeaderCategories = ({ navbarData }: any) => {
     setShowPopoverIndex(null);
   };
   const popoverBottom = (item: any) => (
-    <Popover
-      id={`popover-${item.label}`}
-      title="Popover bottom"
-      className="p-2 category-popover "
-      onMouseLeave={handleMouseLeave}
-    >
-      <div className="row">
+    <Popover id={`popover-${item.label}`} className="p-2 category-popover shadow rounded" onMouseLeave={handleMouseLeave}>
+      <div className="row ">
         {item?.values?.length > 0 &&
           item?.values !== null &&
-          item?.values.map((itemL2: any, index: number) => (
+          item?.values.map((itemL2: any, index: number) =>{
+            const columnCount = Math.ceil(itemL2?.values?.length / 8);
+            console.log([...Array(columnCount)],'columnCount')
+            return (
             <div className="col">
               <div className="heading-category-l2">{itemL2?.label}</div>
-              <hr />
-              {itemL2?.values?.length > 0 &&
-                itemL2?.values !== null &&
-                itemL2?.values?.map((itemL3: any) => (
-                  <div className="heading-category-l3">{itemL3.label}</div>
-                ))}
+              <hr className='m-1'/>
+              <div className=" col-container">
+              {Array.from({ length: columnCount }, (_, columnIndex) => (
+                    <div key={columnIndex} className="column">
+                      {itemL2?.values
+                        ?.slice(columnIndex * 8, (columnIndex + 1) * 8)
+                        .map((itemL3: any, idx: number) => (
+                          <div key={idx} className="heading-category-l3 p-1">
+                            {itemL3?.label !== undefined ? itemL3?.label : `${idx}`}
+                          </div>
+                        ))}
+                    </div>
+                  ))}
+              </div>
             </div>
-          ))}
+          )})}
       </div>
     </Popover>
   );
   return (
+    <header className=''>
+
     <nav ref={ref}>
-      <div className="heading-container" onMouseLeave={handleMouseLeave}>
+      <div className="heading-container shadow-sm py-2" onMouseLeave={handleMouseLeave}>
         {navbarData !== null &&
           navbarData?.length > 0 &&
           navbarData.map((item: any, index: number) => (
-            <div key={index}>
-              <div
-                className="heading-category-l1"
-                onMouseEnter={(e) => handleMouseEnter(e, index)}
-              >
+            <div key={index} className="header-category-container">
+              <div className={`heading-category-l1 ${showPopoverIndex === index && 'text-danger'}`} onMouseEnter={(e) => handleMouseEnter(e, index)}>
                 {item.label}
               </div>
+              {/* <hr className={`my-2 ${showPopoverIndex === index && 'text-danger'}`} /> */}
               <Overlay
-                show={showPopoverIndex === index}
+                show={showPopoverIndex === index && item?.values?.length > 0}
+                // show={true}
                 target={target}
                 placement="bottom"
                 container={ref.current}
@@ -67,6 +71,7 @@ const HeaderCategories = ({ navbarData }: any) => {
           ))}
       </div>
     </nav>
+    </header>
   );
 };
 
