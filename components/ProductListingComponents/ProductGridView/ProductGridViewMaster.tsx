1c5fd0dc4d6f1d253e1dd9c51ad3c9b71d89bc1b
karan <karan@8848digital.com>
2024-07-16 15:31:30 +0530

    fix: no data found img removed

--- components/ProductListingComponents/ProductGridView/ProductGridViewMaster.tsx
+++ components/ProductListingComponents/ProductGridView/ProductGridViewMaster.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import ProductsGridView from './ProductsGridView';
 import GridViewLoadingComponent from './GridViewLoadingComponent';
-import image from '../../../public/assets/images/no_data_image.jpg';
+// import image from '../../../public/assets/images/no_data_image.jpg';
 
 const ProductGridViewMaster = ({ productListingData, isLoading, handlePaginationBtn }: any) => {
   console.log('productListingData', productListingData);
@@ -26,9 +26,9 @@ const ProductGridViewMaster = ({ productListingData, isLoading, handlePagination
     if (productListingData?.length === 0) {
       return (
         <div className="text-center">
-          <div className="p-3" style={{ fontSize: '40px' }}>
+          {/* <div className="p-3" style={{ fontSize: '40px' }}>
             <Image src={image} width={250} height={250} alt="Error Image" />
-          </div>
+          </div> */}
           <div className="text-center">
             <h2 className="theme-blue">Sorry, No Data Found</h2>
           </div>
