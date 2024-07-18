b94eb4cb22735a54d736e167d5ca47dc1efe8e05
rushikesh2404 <rushikesh@8848digital.com>
2024-07-18 17:08:27 +0530

    feat:hide filter section if filters not available

--- components/ProductListingComponents/FilterView/WebFilters.tsx
+++ components/ProductListingComponents/FilterView/WebFilters.tsx
@@ -1,8 +1,7 @@
-import useProductListingFilterHook from '../../../hooks/ProductListPageHooks/useProductListFilterHook';
-import ComponentErrorHandler from '../../ComponentErrorHandler';
+import useProductListingFilterHook from '../../../hooks/product-listing-hooks/product-listing-filter-hook';
 import FilterViewLoadingComponent from './FilterViewLoadingComponent';
 
-const WebFilters = () => {
+const WebFilters = ({ hideFilterSection, setHideFilterSection }: any) => {
   const { filtersData, isLoading, errorMessage, handleFilterCheckFun, selectedFilters } = useProductListingFilterHook();
 
   const showFilterSection: any = () => {
@@ -11,42 +10,50 @@ const WebFilters = () => {
     }
 
     if (Object.keys(filtersData)?.length > 0) {
+      setHideFilterSection(false);
+
       return (
-        <div className="vh-100">
-          <h4>Filters</h4>
-          {filtersData?.filters?.length > 0 &&
-            filtersData?.filters.map((data: any, index: any) => (
-              <>
-                <div key={index}>
-                  <h6 className="mt-4 text-uppercase">{data.section}</h6>
-                  <hr className="my-1" />
-                  {data.values?.length > 0 &&
-                    data.values.map((items: any) => (
-                      <div className="form-check">
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          value={items}
-                          name={data.section}
-                          id={data.section}
-                          onChange={handleFilterCheckFun}
-                          checked={
-                            selectedFilters?.length > 0 && selectedFilters.some((filter: any) => filter.name === data.section && filter.value.includes(items))
-                          }
-                        />
-                        <label className="form-check-label" htmlFor="flexCheckChecked">
-                          {items}
-                        </label>
-                      </div>
-                    ))}
-                </div>
-              </>
-            ))}
-        </div>
+        <>
+          {!hideFilterSection && (
+            <div className="vh-100 p-3" id="sidebar">
+              <h4>Filters</h4>
+              {filtersData?.filters?.length > 0 &&
+                filtersData?.filters.map((data: any, index: any) => (
+                  <>
+                    <div key={index}>
+                      <h6 className="mt-4 text-uppercase">{data.section}</h6>
+                      <hr className="my-1" />
+                      {data.values?.length > 0 &&
+                        data.values.map((items: any) => (
+                          <div className="form-check">
+                            <input
+                              className="form-check-input"
+                              type="checkbox"
+                              value={items}
+                              name={data.section}
+                              id={data.section}
+                              onChange={handleFilterCheckFun}
+                              checked={
+                                selectedFilters?.length > 0 &&
+                                selectedFilters.some((filter: any) => filter.name === data.section && filter.value.includes(items))
+                              }
+                            />
+                            <label className="form-check-label" htmlFor="flexCheckChecked">
+                              {items}
+                            </label>
+                          </div>
+                        ))}
+                    </div>
+                  </>
+                ))}
+            </div>
+          )}
+        </>
       );
     }
-    if (errorMessage && isLoading === false) {
-      return <ComponentErrorHandler error={errorMessage} />;
+
+    if (errorMessage && !isLoading) {
+      setHideFilterSection(true);
     }
   };
   return <>{showFilterSection()}</>;
--- components/ProductListingComponents/ProductListingMaster.tsx
+++ components/ProductListingComponents/ProductListingMaster.tsx
@@ -1,28 +1,27 @@
-import React, { useEffect } from 'react';
-import useProductListing from '../../hooks/ProductListPageHooks/useProductsDataHook';
-import ProductListViewMaster from './ProductListView/ProductListViewMaster';
+import React, { useEffect, useState } from 'react';
 import WebFilters from './FilterView/WebFilters';
 import ProductGridViewMaster from './ProductGridView/ProductGridViewMaster';
+import useProductListing from '../../hooks/product-listing-hooks/product-listing-hook';
 
 const ProductListingMaster = () => {
   const { productListingData, isLoading, handlePaginationBtn } = useProductListing();
+  const [hideFilterSection, setHideFilterSection] = useState<boolean>(false);
 
   const handleDisplayOfProductsList = () => {
-    return (
-      <ProductGridViewMaster productListingData={productListingData} isLoading={isLoading} handlePaginationBtn={handlePaginationBtn} />
-    );
+    return <ProductGridViewMaster productListingData={productListingData} isLoading={isLoading} handlePaginationBtn={handlePaginationBtn} />;
   };
 
   return (
     <div className="mt-5">
-      <section className="listing-page ">
+      <section className="listing-page">
         <div className="container-fuild">
           <div className="d-flex ps-lg-5 pe-lg-4">
-          <div id="sidebar" className=" handle_display_web_filter p-3">
-              <WebFilters />
+            <div>
+              <WebFilters hideFilterSection={hideFilterSection} setHideFilterSection={setHideFilterSection} />
             </div>
+
             <div className="container">
-              <div className="row mt-2 product-listing-row ">{handleDisplayOfProductsList()}</div>
+              <div className="row mt-2 product-listing-row">{handleDisplayOfProductsList()}</div>
             </div>
           </div>
         </div>
@@ -31,5 +30,3 @@ const ProductListingMaster = () => {
   );
 };
 export default ProductListingMaster;
-
-
