ee8d49e59ccfb3e06cbcd09e3c4f785d66e9218a
karan <karan.tamboli2001@gmail.com>
2024-08-07 13:34:23 +0530

    obj destructuring fix

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -23,6 +23,7 @@ const Navbar = () => {
   console.log('cartCount', cartCount);
   console.log('cartCount', cartCount);
   console.log('cartCount', cartCount);
+  console.log('cartCount', cartCount);
   const handleSearch = (e: any) => {
     e.preventDefault();
     router.push('/product-detail/' + searchTerm);
