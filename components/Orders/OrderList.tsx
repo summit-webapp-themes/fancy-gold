da4156113a66940fcd0eba694d15d8221b3d56d8
karan <karan.tamboli2001@gmail.com>
2024-08-09 16:22:43 +0530

    feat: created util file for handling api calls

--- components/Orders/OrderList.tsx
+++ components/Orders/OrderList.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import useOrderListHook from '../../hooks/OrderListHooks/useOrderListHook';
 import image from '../../public/assets/images/no-data.svg';
 import ListingTable from './ListingTable';
+import ComponentErrorHandler from '../ComponentErrorHandler';
 
 const OrderList = () => {
   const { orderListData, isLoading, errorMessage }: any = useOrderListHook();
@@ -13,21 +14,26 @@ const OrderList = () => {
     if (isLoading) {
       return <h2>Loading</h2>;
     }
-    if (orderListData?.length > 0 && orderListData !== null) {
-      return <ListingTable headers={headers} tableData={orderListData} />;
+    if (errorMessage) {
+      return <ComponentErrorHandler error={errorMessage} />;
     }
+    if (orderListData) {
+      if (orderListData?.length > 0) {
+        return <ListingTable headers={headers} tableData={orderListData} />;
+      }
 
-    if (isLoading === false && orderListData?.length === 0) {
-      return (
-        <div className={`text-center `}>
-          <div className="p-3" style={{ fontSize: '40px' }}>
-            <Image src={image} width={200} height={200} alt="Error Image" />
+      if (orderListData?.length === 0) {
+        return (
+          <div className={`text-center `}>
+            <div className="p-3" style={{ fontSize: '40px' }}>
+              <Image src={image} width={200} height={200} alt="Error Image" />
+            </div>
+            <div className="text-center">
+              <h2 className="theme-blue">Sorry, No Data Found</h2>
+            </div>
           </div>
-          <div className="text-center">
-            <h2 className="theme-blue">Sorry, No Data Found</h2>
-          </div>
-        </div>
-      );
+        );
+      }
     }
   };
 
--- components/ProductListingComponents/HorizontalFilterList.tsx/Purity.tsx
+++ components/ProductListingComponents/HorizontalFilterList.tsx/Purity.tsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 import { get_access_token } from '../../../store/slices/auth/token-login-slice';
 import { CONSTANTS } from '../../../services/config/app-config';
-import { callGetAPI } from '../../../utils/utils';
+import { callGetAPI } from '../../../utils/http-methods';
 import horizontalFilterStyles from '../../../styles/components/horizontalFilter.module.scss';
 
 const Purity = () => {
