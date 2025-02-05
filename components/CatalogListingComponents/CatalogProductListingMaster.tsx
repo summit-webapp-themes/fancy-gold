42c52a133e431ed7f56ec9c7e9ca584daf2bce5d
NikitaD8848 <nikita.dhumal@8848digital.com>
2025-02-05 12:35:09 +0530

    feat: catalog product listing page added

---
+++ components/CatalogListingComponents/CatalogProductListingMaster.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function CatalogProductListingMaster() {
+  return (
+    <div className="container vh-100">
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <div>
+          <h4>This page is under maintainence</h4>
+          <h5 className="text-center text-secondary">we will be back soon.</h5>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CatalogProductListingMaster;
