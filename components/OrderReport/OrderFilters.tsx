54932349cd15adadfd5bd9b8010c42c545920323
NikitaD8848 <nikita.dhumal@8848digital.com>
2025-02-07 11:36:01 +0530

    feat: filters added for order listing

--- components/OrderReport/OrderFilters.tsx
+++ components/OrderReport/OrderFilters.tsx
@@ -1,11 +1,11 @@
 import Select from 'react-select';
 
-function OrderReportFilters({ data, handleFilterChange }: any) {
+function OrderFilters({ data, handleFilterChange }: any) {
   return (
     <div className="row">
       {data?.length > 0 &&
         data?.map((filter: any, index: number) => (
-          <div className="col-12 col-sm-6 col-md-4 col-lg-3 pe-1 pe-md-3 " key={index}>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3  " key={index}>
             <div>
               <label htmlFor={filter?.label} className="fw-bold">
                 {filter?.label}
@@ -26,4 +26,4 @@ function OrderReportFilters({ data, handleFilterChange }: any) {
   );
 }
 
-export default OrderReportFilters;
+export default OrderFilters;
