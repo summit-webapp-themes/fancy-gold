df5f501a17e29479336795c12de755bb52c767db
NikitaD8848 <nikita.dhumal@8848digital.com>
2025-02-17 18:24:38 +0530

    fix: resolving conflicts

--- components/ProductListingComponents/ProductGridView/ProductGridViewMaster.tsx
+++ components/ProductListingComponents/ProductGridView/ProductGridViewMaster.tsx
@@ -14,6 +14,7 @@ const ProductGridViewMaster = ({
   cartData,
 }: any) => {
   const { query } = useRouter();
+
   const pageOffset = query?.page ? Number(query?.page) - 1 : 0;
 
   const handlePageClick = (event: any) => {
