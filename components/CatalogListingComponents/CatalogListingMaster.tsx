1dbb5ea7d8709f697f9e2bb9fb9f9077f7f6a411
NikitaD8848 <nikita.dhumal@8848digital.com>
2025-02-18 16:33:03 +0530

    feat: user authentication added

--- components/CatalogListingComponents/CatalogListingMaster.tsx
+++ components/CatalogListingComponents/CatalogListingMaster.tsx
@@ -2,17 +2,20 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { FaArrowRightLong } from 'react-icons/fa6';
 import { useSelector } from 'react-redux';
+import noImage from '../../public/assets/images/no_image.png';
 import { CONSTANTS } from '../../services/config/app-config';
 import { get_access_token } from '../../store/slices/auth/token-login-slice';
-import { callGetAPI } from '../../utils/http-methods';
 import styles from '../../styles/components/catalog.module.scss';
-import { FaArrowRightLong } from 'react-icons/fa6';
-import noImage from '../../public/assets/images/no_image.png';
+import { callGetAPI } from '../../utils/http-methods';
+import ErrorStyles from '../../styles/components/errorboundary.module.scss';
+import image from '../../public/assets/images/api_error_img.webp';
 
 function CatalogListingMaster() {
   const router = useRouter();
   const TokenFromStore: any = useSelector(get_access_token);
+  const isCatalogUser = localStorage.getItem('isCatalogUser');
   const [catalogList, setCatalogList] = useState([]);
   const getCatalogListValues = async () => {
     const params = {
@@ -35,50 +38,72 @@ function CatalogListingMaster() {
   const imageLoader = ({ src, width, quality }: any) => {
     return `${CONSTANTS.API_BASE_URL}${src}?w=${width}&q=${quality || 75}`;
   };
-  console.log(catalogList, 'data111');
   useEffect(() => {
     fetchCatalogListValues();
     router.push({
       query: { page: 1 },
     });
   }, []);
-  return (
-    <div className="container">
-      <div className="mt-5 text-center fw-bold">
-        <h1>Catalog List</h1>
-      </div>
-      <div className="row mt-2">
-        {catalogList?.length > 0 &&
-          catalogList?.map((catalog: any, index: any) => (
-            <div className="col-lg-3 col-md-4 col-6 position-relative mt-3">
-              {catalog?.image !== '' && catalog?.image !== null ? (
-                <div className={styles.catalog_img}>
-                  <Image
-                    src={catalog?.image}
-                    alt="catalog-image"
-                    className="img-fluid w-100 h-100 rounded"
-                    width={100}
-                    height={100}
-                    loader={imageLoader}
-                  />
-                </div>
-              ) : (
-                <div className={styles.catalog_img}>
-                  <Image src={noImage} alt="catalog-image" className=" w-100 h-100 rounded" width={100} height={100} />
-                </div>
-              )}
-              <div className="d-flex justify-content-center">
-                <Link href={{ pathname: `/catalog/${catalog?.slug}`, query: { page: 1 } }} className={`${styles.catalog_list_btn}`}>
-                  <span>{catalog?.name}</span>
-                  <span className="ps-2">
-                    <FaArrowRightLong />
-                  </span>
-                </Link>
-              </div>
+  const handleDataRendering = () => {
+    if (isCatalogUser !== 'true') {
+      return (
+        <div className="d-flex justify-content-center">
+          <div className={`${ErrorStyles.error_content}`}>
+            <div className="p-3" style={{ fontSize: '40px' }}>
+              <Image src={image} width={250} height={250} alt="Error Image" />
             </div>
-          ))}
-      </div>
-    </div>
-  );
+            <h4 data-text="Access Denied!">Access Denied!</h4>
+            <p className="text-secondary">
+              You do not have permission to view this page.
+              <br />
+              Please check your credentials and try again.
+            </p>
+            <button className="btn btn-primary border-0" style={{ backgroundColor: '#ff3b3b' }} onClick={() => router.push('/')}>
+              Home Page
+            </button>
+          </div>
+        </div>
+      );
+    } else
+      return (
+        <div className="container mb-5">
+          <div className="mt-5 text-center fw-bold">
+            <h1>Catalog List</h1>
+          </div>
+          <div className="row mt-2">
+            {catalogList?.length > 0 &&
+              catalogList?.map((catalog: any, index: any) => (
+                <div className="col-lg-3 col-md-4 col-6 position-relative mt-3">
+                  {catalog?.image !== '' && catalog?.image !== null ? (
+                    <div className={styles.catalog_img}>
+                      <Image
+                        src={catalog?.image}
+                        alt="catalog-image"
+                        className="img-fluid w-100 h-100 rounded"
+                        width={100}
+                        height={100}
+                        loader={imageLoader}
+                      />
+                    </div>
+                  ) : (
+                    <div className={styles.catalog_img}>
+                      <Image src={noImage} alt="catalog-image" className=" w-100 h-100 rounded" width={100} height={100} />
+                    </div>
+                  )}
+                  <div className="d-flex justify-content-center">
+                    <Link href={{ pathname: `/catalog/${catalog?.slug}`, query: { page: 1 } }} className={`${styles.catalog_list_btn}`}>
+                      <span>{catalog?.name}</span>
+                      <span className="ps-2">
+                        <FaArrowRightLong />
+                      </span>
+                    </Link>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      );
+  };
+  return <>{handleDataRendering()}</>;
 }
 export default CatalogListingMaster;
