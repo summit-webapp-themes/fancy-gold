70970e2e8e704d57033c894faa28507dfd2fe29b
rushikesh24 <rushikesh@8848digital.com>
2025-07-11 14:59:14 +0530

    feat: autologout if user do not used site for 1 hr

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,12 @@
 import { useRouter } from 'next/router';
 import Navbar from './Navbar/Navbar';
+import useInactivityLogout from '../hooks/GeneralHooks/useInactivityLogout';
 
 const Layout = ({ children }: any) => {
   const router = useRouter();
+
+  useInactivityLogout();
+
   const toShowHeader =
     router.pathname === '/login' || router.pathname === '/register' || router.pathname === '/forgot_password' ? false : true;
   const toShowFooter =
