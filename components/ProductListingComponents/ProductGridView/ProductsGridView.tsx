65650140321bc4f23c3b64bcebc97dd4d8c9906a
8848Digital-Rohit-Frontend <rohit.g@8848digital.com>
2025-10-27 13:48:48 +0530

    feat: pagination added to the order listing page :rg

--- components/ProductListingComponents/ProductGridView/ProductsGridView.tsx
+++ components/ProductListingComponents/ProductGridView/ProductsGridView.tsx
@@ -1,6 +1,7 @@
 import ReactPaginate from 'react-paginate';
 import ProductCard from '../../../cards/ProductCard';
 import paginationStyle from '../../../styles/components/pagination.module.scss';
+import Pagination from '../../Paginate/Pagination';
 
 const ProductsGridView = ({
   productListingData,
@@ -10,28 +11,29 @@ const ProductsGridView = ({
   handleShow,
   wishlistData,
   cartData,
-  sortBy
+  sortBy,
 }: any) => {
   const isNextButtonDisabled: boolean = parseInt((productListTotalCount / 12).toString(), 10) === pageOffset;
   return (
     <>
       {productListingData.map((item: any, index: any) => (
         <div key={index} className="col-sm-6 col-lg-4 col-xl-3 col-xxl-3 text-center mb-4">
-          <ProductCard data={item} handleShow={handleShow} wishlistData={wishlistData} btnAction={'Add'} cartData={cartData}  index={index} sortBy={sortBy} />
+          <ProductCard
+            data={item}
+            handleShow={handleShow}
+            wishlistData={wishlistData}
+            btnAction={'Add'}
+            cartData={cartData}
+            index={index}
+            sortBy={sortBy}
+          />
         </div>
       ))}
-      <ReactPaginate
-        previousLabel={'Prev'}
-        nextLabel={'Next'}
-        pageCount={productListTotalCount / 12}
-        pageRangeDisplayed={3}
-        onPageChange={handlePageClick}
-        containerClassName={`${paginationStyle.paginationBttns}`}
-        previousLinkClassName={pageOffset === 0 ? paginationStyle.paginationDisabled : paginationStyle.previousBttn}
-        disabledClassName={paginationStyle.paginationDisabled}
-        nextLinkClassName={isNextButtonDisabled ? paginationStyle.paginationDisabled : `${paginationStyle.nextBttn}`}
-        activeClassName={`${paginationStyle.paginationActive}`}
-        forcePage={pageOffset}
+      <Pagination
+        totalCount={productListTotalCount}
+        handlePageClick={handlePageClick}
+        pageOffset={pageOffset}
+        isNextButtonDisabled={isNextButtonDisabled}
       />
     </>
   );
